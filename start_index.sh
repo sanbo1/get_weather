4fde4ad1c4009c8863c88d0aa8e5f72bc469a536
sanbo1 <sanbo.dev@gmail.com>
2016-02-13 22:05:15 +0900

    定期実行対応

---
+++ start_index.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+nohup node /home/pi/get_weather/index.js &
+
---
+++ stop_index.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#nohup node /home/pi/get_weather/index.js &
+
+FOREVER="./node_modules/forever/bin/forever"
+
+sudo ${FOREVER} stop index.js && sudo ${FOREVER} list
+
